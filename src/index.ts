43cc2d074dd65dc63d7ad9f8bb490657f7a19c37
你的GitHub用户名 <GitHub>
2025-12-25 17:17:56 +0800

    feat: add test route and update index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-
 export default {
   async fetch(req: Request, env: any) {
     const path = new URL(req.url).pathname;
-    if(path === '/api/test')
-    return path
 
+    if (path === "/api/test") {
+      return new Response(path, {
+        headers: { "content-type": "text/plain; charset=utf-8" },
+      });
+    }
+
+    return new Response("Not Found", { status: 404 });
   },
 };
