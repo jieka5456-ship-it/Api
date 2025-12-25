9ac9402f0fdf36aefa65a36578a7ffb44b175267
你的GitHub用户名 <GitHub>
2025-12-25 17:15:54 +0800

    feat: add test route and update index

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,9 @@
+
 export default {
   async fetch(req: Request, env: any) {
-    // 只处理 GET 请求
-    if (req.method === "GET") {
-      return new Response(
-        JSON.stringify({
-          code: 888,
-          message: "test ok",
-        }),
-        {
-          status: 200,
-          headers: {
-            "content-type": "application/json; charset=utf-8",
-          },
-        }
-      );
-    }
+    const path = new URL(req.url).pathname;
+    if(path === '/api/test')
+    return path
 
-    // 其他请求兜底
-    return new Response(
-      JSON.stringify({
-        code: 405,
-        message: "Method Not Allowed",
-      }),
-      {
-        status: 405,
-        headers: {
-          "content-type": "application/json; charset=utf-8",
-        },
-      }
-    );
   },
 };
