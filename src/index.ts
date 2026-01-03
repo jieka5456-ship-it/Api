cf1f168c1be14a72589fe69c4aafa221eb40e711
你的GitHub用户名 <GitHub>
2026-01-03 23:44:57 +0800

    更新内容说明

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,8 @@ import { JsonFail } from './Message'
 export default {
   async fetch(req: Request, env: any) {
     // 解析请求体
-    let body
-    try {
-      body = req.json()
-    } catch (error) {
-      return JsonFail(400, 'Invalid JSON format')
-    }
-    if (!body) return JsonFail(201, 'Parameters are missing')
+    const body = req.json()
     
-    return JsonFail(200,'Request processed successfully');
+    return JsonFail(200,'33333');
   }
 };
