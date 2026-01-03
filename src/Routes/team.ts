5646387dff2b957f1d0b0e92da65eafa3f3b545c
你的GitHub用户名 <GitHub>
2026-01-04 00:13:56 +0800

    更新内容说明

--- src/Routes/team.ts
+++ src/Routes/team.ts
@@ -1,2 +1,9 @@
 //team.ts
 //团队项目Api
+import { JsonFail } from '../Message'
+export async function handleTeam(req: Request, env: any) {
+  const Body = req.json();
+
+
+  return JsonFail(100,"800");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 
 import { JsonFail } from './Message'
+import {handleTeam} from './Routes/team'
 
 export default {
   async fetch(req: Request, env: any) {
     const url = new URL(req.url);
-    if (url.pathname.startsWith('/team')) return new Response('Not 111', { status: 404 });
+    if (url.pathname.startsWith('/team')) return handleTeam(req,env);
     return new Response('222', { status: 404 });
   }
 };
