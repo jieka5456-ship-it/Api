4dac8fceec4677053f3b8ef17356222986a02893
你的GitHub用户名 <GitHub>
2026-01-26 14:03:19 +0800

    更新内容说明

--- src/Telegram/index.ts
+++ src/Telegram/index.ts
@@ -1,30 +1,61 @@
 
 import { JsonOk } from "../Message";
-const BOT_TOKEN = "8257891631:AAGbEYI-YzuhpvUpcM8KsbxsJcVeit79Ymo"
+const BOT_TOKEN = "8257891631:AAGbEYI-1936YzuhpvUpcM8KsbxsJcVeit79Ymo"
 export async function TgPost(Req: any, Env: any) {
-  const update: any = await Req.json().catch(() => null);
-  console.log("TG_UPDATE:", JSON.stringify(update));
+    const update: any = await Req.json().catch(() => null);
+    console.log("TG_UPDATE:", JSON.stringify(update));
 
-  const chatId = update?.message?.chat?.id;
-  const text = update?.message?.text ?? "";
-  console.log("chatId =", chatId, "text =", text);
+    const chatId = update?.message?.chat?.id;
+    const text = update?.message?.text ?? "";
 
-  if (!chatId) {
-    console.log("No chatId. Maybe callback_query or other update type.");
-    return JsonOk({ ok: true, reason: "no chatId" }, 200);
-  }
+    //先判断用户ID是否存在
+    if (!chatId) {
+        console.log("No chatId. Maybe callback_query or other update type.");
+        return JsonOk({ ok: true, reason: "no chatId" }, 200);
+    }
 
-  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
-  const payload = { chat_id: chatId, text: `收到：${text || "(非文本消息)"}` };
+    // 判断文本是否存在
+    let json: any = null;
+    try {
+        json = JSON.parse(text);
+    } catch (e) {
+        json = null;
+    }
+    if (!json) {
+        return return_TXT(chatId, "JSON格式错误")
+    }
+    const Tmss = await Admin123(json)
+    const Tmjson:any = await Tmss.json()
+    if (Tmjson.code == 200) {
+        return return_TXT(chatId, "处理成功")
+    }else{
+        return return_TXT(chatId, `"处理失败:${Tmjson.data.data.detail}`)
+    }
 
-  const resp = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-  });
+}
+// 统一回复信息
+async function return_TXT( id: any,Txt: string) {
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+    const payload = { chat_id: id, text: Txt };
+    const resp = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+    });
+    const data = await resp.json().catch(() => ({}));
+    return JsonOk({ ok: true, tg: data }, 200);
+}
 
-  const data = await resp.json().catch(() => ({}));
-  console.log("sendMessage status:", resp.status, "resp:", JSON.stringify(data));
-
-  return JsonOk({ ok: true, tg: data }, 200);
+//调用API接口处理Team
+async function Admin123(params: any) {
+    const apiUrl = "https://pyapi.my91.my/TeamAdmin123";
+    return await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            session: params, 
+        }),
+    });
 }
