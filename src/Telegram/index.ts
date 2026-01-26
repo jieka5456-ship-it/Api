c87a10ec01e1f138353f627752eee352102e61f0
你的GitHub用户名 <GitHub>
2026-01-26 12:23:48 +0800

    更新内容说明

--- src/Telegram/index.ts
+++ src/Telegram/index.ts
@@ -3,9 +3,18 @@ import { JsonOk } from "../Message";
 
 const BOT_TOKEN = "8257891631:AAGbEYI-YzuhpvUpcM8KsbxsJcVeit79Ymo"
 export async function TgPost(Req:any,Env:any) {
-    
-    const update = await Req.json().catch(() => null);
-    console.log("TG_UPDATE_RAW:", JSON.stringify(update));
-
-    return JsonOk(update, 200)
+    const update: any = await Req.json();
+    const chatId = update?.message?.chat?.id;
+    const text = update?.message?.text ?? "";
+    if(chatId){
+        await fetch(`https://api.telegram.org/bot${Env.BOT_TOKEN}/sendMessage`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+            chat_id: chatId,
+            text: `收到：${text || "(非文本消息)"}`,
+            }),
+        });
+    }
+    return JsonOk({ ok: true }, 200);
 }
